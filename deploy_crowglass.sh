46d372da7323944a4f6fad96095223bb19d4e6e5
James Williams <james@jameswilliams.me>
2022-05-01 16:57:29 -0400

    Enable TLS on crowglass.com redirect

--- deploy_crowglass.sh
+++ deploy_crowglass.sh
@@ -29,13 +29,13 @@ fi
   CMD+="docker run -d --name redirect_${SITE} "
   CMD+="--label 'traefik.http.routers.${SITE}.entrypoints=websecure' "
   CMD+="--label 'traefik.http.routers.${SITE}.rule=Host(\`${SITE_HOST}\`)' "
-  #CMD+="--label 'traefik.http.routers.${SITE}.tls=true' "
-  #CMD+="--label 'traefik.http.routers.${SITE}.tls.certresolver=le' "
+  CMD+="--label 'traefik.http.routers.${SITE}.tls=true' "
+  CMD+="--label 'traefik.http.routers.${SITE}.tls.certresolver=le' "
 
   CMD+="--label 'traefik.http.routers.${SITE}_redirect.entrypoints=web' "
   CMD+="--label 'traefik.http.routers.${SITE}_redirect.rule=Host(\`${SITE_HOST}\`)' "
-  #CMD+="--label 'traefik.http.routers.${SITE}_redirect.middlewares=${SITE}_redirect' "
-  #CMD+="--label 'traefik.http.middlewares.${SITE}_redirect.redirectscheme.scheme=https' "
+  CMD+="--label 'traefik.http.routers.${SITE}_redirect.middlewares=${SITE}_redirect' "
+  CMD+="--label 'traefik.http.middlewares.${SITE}_redirect.redirectscheme.scheme=https' "
   
   CMD+="-e REDIRECT_TARGET=crowglassdesign.com "
   CMD+="willia4/docker-web-redirect:0.0.1 "
